18b39cc3735170b8f7f985153bd5865721c19b2e
Web4 <167559384+Web4application@users.noreply.github.com>
2025-01-04 18:09:36 -0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import AIFunctions from '../components/AIFunctions';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Web4asset</h1>
+      <AIFunctions />
+    </div>
+  );
+}
